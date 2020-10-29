71c636855b5ce51170180a8aff6be49c7bc0be65
Ilya Protasov <ilya.protasov.22@gmail.com>
2020-10-30 00:36:53 +0300

    Added HttpHandler class and test for it

---
+++ SecureDataCleanerLibrary/HttpHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SecureDataCleanerLibrary.Models;
+
+namespace SecureDataCleanerLibrary
+{
+    public class HttpHandler
+    {
+        HttpResult _currentLog;
+        public HttpResult CurrentLog { get { return _currentLog; } }
+
+        public string Process(string url, string body, string response, List<SecureDataInfo> secureDataInfoList)
+        {
+            var httpResult = new HttpResult
+            {
+                Url = url,
+                RequestBody = body,
+                ResponseBody = response
+            };
+
+            //очищаем secure данные в httpResult, либо создаем новый clearedHttpResult на основе httpResult
+            var secureDataCleaner = new SecureDataCleaner(secureDataInfoList);
+            httpResult = secureDataCleaner.CleanHttpResult(httpResult);
+
+            Log(httpResult);
+
+            return response;
+        }
+
+        /// <summary>
+        /// Логирует данные запроса, они должны быть уже без данных которые нужно защищать 
+        /// </summary>
+        /// <param name="result"></param>
+        protected void Log(HttpResult result)
+        {
+            _currentLog = new HttpResult
+            {
+                Url = result.Url,
+                RequestBody = result.RequestBody,
+                ResponseBody = result.ResponseBody
+            };
+        }
+    }
+
+}
---
+++ SecureDataCleanerLibraryTests/HttpHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using SecureDataCleanerLibrary;
+using SecureDataCleanerLibrary.Models;
+using SecureDataCleanerLibrary.Models.Enums;
+using Xunit;
+
+namespace SecureDataCleanerLibraryTests
+{
+    public class HttpHandlerTests
+    {
+        [Fact]
+        public void HttpHandler_Process_BookingcomHttpResult_ClearSecureData()
+        {
+            //Arrange
+            var bookingcomHttpResult = new HttpResult
+            {
+                Url = "http://test.com/users/max/info?pass=123456",
+                RequestBody = "http://test.com?user=max&pass=123456",
+                ResponseBody = "http://test.com?user=max&pass=123456"
+            };
+            var httpHandler = new HttpHandler();
+
+            var dataLocationQuery = new HashSet<SecureDataLocation> { SecureDataLocation.UrlQuery };
+            var dataLocationRest = new HashSet<SecureDataLocation> { SecureDataLocation.UrlRest };
+
+            var secureKey1 = "user";
+            var locationsInfo1 = new Dictionary<PropertyType, HashSet<SecureDataLocation>>();
+            locationsInfo1.Add(PropertyType.RequestBody, dataLocationQuery);
+            locationsInfo1.Add(PropertyType.ResponseBody, dataLocationQuery);
+            var secureDataInfo1 = new SecureDataInfo
+            {
+                SecureKey = secureKey1,
+                LocationsInfo = locationsInfo1
+            };
+
+            var secureKey2 = "pass";
+            var locationsInfo2 = new Dictionary<PropertyType, HashSet<SecureDataLocation>>();
+            locationsInfo2.Add(PropertyType.Url, dataLocationQuery);
+            locationsInfo2.Add(PropertyType.RequestBody, dataLocationQuery);
+            locationsInfo2.Add(PropertyType.ResponseBody, dataLocationQuery);
+            var secureDataInfo2 = new SecureDataInfo
+            {
+                SecureKey = secureKey2,
+                LocationsInfo = locationsInfo2
+            };
+
+            var secureKey3 = "users";
+            var locationsInfo3 = new Dictionary<PropertyType, HashSet<SecureDataLocation>>();
+            locationsInfo3.Add( PropertyType.Url, dataLocationRest);
+            var secureDataInfo3 = new SecureDataInfo
+            {
+                SecureKey = secureKey3,
+                LocationsInfo = locationsInfo3
+            };
+
+            var secureDataInfoList = new List<SecureDataInfo>();
+            secureDataInfoList.Add(secureDataInfo1);
+            secureDataInfoList.Add(secureDataInfo2);
+            secureDataInfoList.Add(secureDataInfo3);
+
+            //Act
+            httpHandler.Process(bookingcomHttpResult.Url, bookingcomHttpResult.RequestBody, bookingcomHttpResult.ResponseBody, secureDataInfoList);
+
+            //Assert
+            Assert.Equal("http://test.com/users/XXX/info?pass=XXXXXX", httpHandler.CurrentLog.Url);
+            Assert.Equal("http://test.com?user=XXX&pass=XXXXXX", httpHandler.CurrentLog.RequestBody);
+            Assert.Equal("http://test.com?user=XXX&pass=XXXXXX", httpHandler.CurrentLog.ResponseBody);
+        }
+    }
+}
